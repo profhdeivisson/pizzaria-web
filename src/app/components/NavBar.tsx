9c53940f8ca6e3ace2447202616dabb42597414c
josuesoares64 <josuesoares318@gmail.com>
2025-09-15 20:39:48 -0300

    Ajuste NavBar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,11 +1,26 @@
 import Link from 'next/link'
+import Image from 'next/image';
 
 function NavBar() {
   return (
-    <nav className="fixed top-0 w-full flex items-center py-2 px-2 justify-between z-50 ">
-      <Link href="/" className="uppercase font-black text-lg h-12 items-center text-orange-500">
-        Bella<span className='text-yellow-500'>Pizza</span>
+    <nav className="fixed top-0 w-full flex bg-red-600 items-center py-2 px-3 justify-between z-50 text-white">
+      
+      <Link href="/" className="flex items-center gap-2 uppercase font-black text-lg">
+        <Image 
+          src="/img/logo.png" 
+          alt="Logo Bella Pizza" 
+          width={32}
+          height={32}
+          className="object-contain rounded-full"
+        />
+        <span>
+          Bella<span className='text-yellow-500'>Pizza</span>
+        </span>
       </Link>
+
+      <div className="flex items-center gap-4">
+      </div>
+
     </nav>
   );
 }
